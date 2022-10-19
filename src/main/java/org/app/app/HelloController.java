b27e86304aad036c5f4661f10013f3eb49291a59
Matijas22001 <matekaw545@gmail.com>
2022-10-19 12:57:10 +0200

    backbone view done

--- src/main/java/org/app/app/HelloController.java
+++ src/main/java/org/app/app/HelloController.java
@@ -2,13 +2,17 @@ package org.app.app;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+    @FXML private ImageView exitButton;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private void closeButtonAction(){
+        // get a handle to the stage
+        Stage stage = (Stage) exitButton.getScene().getWindow();
+        // do what you have to do
+        stage.close();
     }
 }
