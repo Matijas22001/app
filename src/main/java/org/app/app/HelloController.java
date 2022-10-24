faadbbe83b47ccfa69db429c370443a0f5c6e522
Matijas22001 <matekaw545@gmail.com>
2022-10-24 12:32:21 +0200

    most work done

--- src/main/java/org/app/app/HelloController.java
+++ src/main/java/org/app/app/HelloController.java
@@ -3,19 +3,13 @@ package org.app.app;
 import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.input.ScrollEvent;
-import javafx.scene.input.TouchEvent;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
-import java.io.Console;
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
 import java.time.LocalDateTime;
@@ -30,18 +24,15 @@ public class HelloController {
     @FXML private ImageView sub_bar_btn;
     @FXML private ImageView sub_hz_btn;
     @FXML private ImageView sub_hit_btn;
-
     @FXML private ImageView iv_border_1;
     @FXML private ImageView iv_border_2;
     @FXML private ImageView iv_border_3;
-
     @FXML private TextField bar_tv;
     @FXML private TextField hz_tv;
     @FXML private TextField hit_tv;
     @FXML private TextField sum_hit_tv;
     @FXML
     private TextField time_tv;
-
     private Timeline watcher_timeline;
 
     @FXML
@@ -65,7 +56,6 @@ public class HelloController {
 
     @FXML
     private void add_bar_value(){
-        System.out.println("Added from onetime");
         iv_border_1.setVisible(true);
         iv_border_2.setVisible(false);
         iv_border_3.setVisible(false);
@@ -88,8 +78,7 @@ public class HelloController {
     private void add_bar_value_sequence() {
         add_bar_btn.addEventHandler(MouseEvent.MOUSE_EXITED_TARGET, mouseEvent -> stopWatcher());
         if(watcher_timeline == null){
-            watcher_timeline = new Timeline(new KeyFrame(Duration.seconds(0.5), actionEvent -> {
-                System.out.println("Added from sequence");
+            watcher_timeline = new Timeline(new KeyFrame(Duration.seconds(0.2), actionEvent -> {
                 iv_border_1.setVisible(true);
                 iv_border_2.setVisible(false);
                 iv_border_3.setVisible(false);
@@ -106,10 +95,84 @@ public class HelloController {
     }
 
     @FXML
-    private void sub_bar_value_sequence() {
-        add_bar_btn.addEventHandler(MouseEvent.MOUSE_EXITED_TARGET, mouseEvent -> stopWatcher());
+    private void add_hz_value(){
+        iv_border_1.setVisible(false);
+        iv_border_2.setVisible(true);
+        iv_border_3.setVisible(false);
+        int hzValue = Integer.parseInt(hz_tv.getText());
+        if(hzValue<20){
+            hz_tv.setText(String.valueOf(hzValue+1));
+        }
+    }
+
+    @FXML
+    private void add_hz_value_sequence(){
+        add_hz_btn.addEventHandler(MouseEvent.MOUSE_EXITED_TARGET, mouseEvent -> stopWatcher());
         if(watcher_timeline == null){
-            watcher_timeline = new Timeline(new KeyFrame(Duration.seconds(0.5), actionEvent -> {
+            watcher_timeline = new Timeline(new KeyFrame(Duration.seconds(0.2), actionEvent -> {
+                iv_border_1.setVisible(false);
+                iv_border_2.setVisible(true);
+                iv_border_3.setVisible(false);
+                int hzValue = Integer.parseInt(hz_tv.getText());
+                if(hzValue<20){
+                    hz_tv.setText(String.valueOf(hzValue+1));
+                }
+            })
+            );
+            watcher_timeline.setCycleCount(Timeline.INDEFINITE);
+            watcher_timeline.play();
+        }
+    }
+
+    @FXML
+    private void add_hit_value(){
+        iv_border_1.setVisible(false);
+        iv_border_2.setVisible(false);
+        iv_border_3.setVisible(true);
+        int hitValue = Integer.parseInt(hit_tv.getText());
+        if(hitValue<3000){
+            hit_tv.setText(String.valueOf(hitValue+100));
+            sum_hit_tv.setText(String.valueOf(hitValue+100));
+        }
+    }
+
+    @FXML
+    private void add_hit_value_sequence(){
+        add_hit_btn.addEventHandler(MouseEvent.MOUSE_EXITED_TARGET, mouseEvent -> stopWatcher());
+        if(watcher_timeline == null){
+            watcher_timeline = new Timeline(new KeyFrame(Duration.seconds(0.2), actionEvent -> {
+                iv_border_1.setVisible(false);
+                iv_border_2.setVisible(false);
+                iv_border_3.setVisible(true);
+                int hitValue = Integer.parseInt(hit_tv.getText());
+                if(hitValue<3000){
+                    hit_tv.setText(String.valueOf(hitValue+100));
+                    sum_hit_tv.setText(String.valueOf(hitValue+100));
+                }
+            })
+            );
+            watcher_timeline.setCycleCount(Timeline.INDEFINITE);
+            watcher_timeline.play();
+        }
+    }
+
+    @FXML
+    private void sub_bar_value(){
+        iv_border_1.setVisible(true);
+        iv_border_2.setVisible(false);
+        iv_border_3.setVisible(false);
+        double barValue = Double.parseDouble(bar_tv.getText());
+        DecimalFormat df = new DecimalFormat("#.#", DecimalFormatSymbols.getInstance(Locale.US));
+        if(barValue>0){
+            bar_tv.setText(String.valueOf(df.format(barValue-0.1)));
+        }
+    }
+
+    @FXML
+    private void sub_bar_value_sequence() {
+        sub_bar_btn.addEventHandler(MouseEvent.MOUSE_EXITED_TARGET, mouseEvent -> stopWatcher());
+        if(watcher_timeline == null){
+            watcher_timeline = new Timeline(new KeyFrame(Duration.seconds(0.2), actionEvent -> {
                 iv_border_1.setVisible(true);
                 iv_border_2.setVisible(false);
                 iv_border_3.setVisible(false);
@@ -126,80 +189,65 @@ public class HelloController {
     }
 
     @FXML
-    private void add_hz_value(){
-        //if(wasMoveLast){
-        //    wasMoveLast = false;
-        //}else{
-            iv_border_1.setVisible(false);
-            iv_border_2.setVisible(true);
-            iv_border_3.setVisible(false);
-            int hzValue = Integer.parseInt(hz_tv.getText());
-            if(hzValue<20){
-                hz_tv.setText(String.valueOf(hzValue+1));
-            }
-        //}
-    }
-
-    @FXML
-    private void add_hit_value(){
-        //if(wasMoveLast){
-        //    wasMoveLast = false;
-        //}else{
-            iv_border_1.setVisible(false);
-            iv_border_2.setVisible(false);
-            iv_border_3.setVisible(true);
-            int hitValue = Integer.parseInt(hit_tv.getText());
-            if(hitValue<3000){
-                hit_tv.setText(String.valueOf(hitValue+100));
-                sum_hit_tv.setText(String.valueOf(hitValue+100));
-            }
-        //}
-    }
-
-    @FXML
-    private void sub_bar_value(){
-        //if(wasMoveLast){
-        //    wasMoveLast = false;
-        //}else{
-            iv_border_1.setVisible(true);
-            iv_border_2.setVisible(false);
-            iv_border_3.setVisible(false);
-            double barValue = Double.parseDouble(bar_tv.getText());
-            DecimalFormat df = new DecimalFormat("#.#", DecimalFormatSymbols.getInstance(Locale.US));
-            if(barValue>0){
-                bar_tv.setText(String.valueOf(df.format(barValue-0.1)));
-            }
-        //}
-    }
-
-    @FXML
     private void sub_hz_value(){
-        //if(wasMoveLast){
-        //    wasMoveLast = false;
-        //}else{
-            iv_border_1.setVisible(false);
-            iv_border_2.setVisible(true);
-            iv_border_3.setVisible(false);
-            int hzValue = Integer.parseInt(hz_tv.getText());
-            if(hzValue>0){
-                hz_tv.setText(String.valueOf(hzValue-1));
-            }
-        //}
+        iv_border_1.setVisible(false);
+        iv_border_2.setVisible(true);
+        iv_border_3.setVisible(false);
+        int hzValue = Integer.parseInt(hz_tv.getText());
+        if(hzValue>0){
+            hz_tv.setText(String.valueOf(hzValue-1));
+        }
     }
+
+    @FXML
+    private void sub_hz_value_sequence(){
+        sub_hz_btn.addEventHandler(MouseEvent.MOUSE_EXITED_TARGET, mouseEvent -> stopWatcher());
+        if(watcher_timeline == null){
+            watcher_timeline = new Timeline(new KeyFrame(Duration.seconds(0.2), actionEvent -> {
+                iv_border_1.setVisible(false);
+                iv_border_2.setVisible(true);
+                iv_border_3.setVisible(false);
+                int hzValue = Integer.parseInt(hz_tv.getText());
+                if(hzValue>0){
+                    hz_tv.setText(String.valueOf(hzValue-1));
+                }
+            })
+            );
+            watcher_timeline.setCycleCount(Timeline.INDEFINITE);
+            watcher_timeline.play();
+        }
+    }
+
 
     @FXML
     private void sub_hit_value(){
-        //if(wasMoveLast){
-        //    wasMoveLast = false;
-        //}else{
-            iv_border_1.setVisible(false);
-            iv_border_2.setVisible(false);
-            iv_border_3.setVisible(true);
-            int hitValue = Integer.parseInt(hit_tv.getText());
-            if(hitValue>0){
-                hit_tv.setText(String.valueOf(hitValue-100));
-                sum_hit_tv.setText(String.valueOf(hitValue-100));
-            }
-        //}
+        iv_border_1.setVisible(false);
+        iv_border_2.setVisible(false);
+        iv_border_3.setVisible(true);
+        int hitValue = Integer.parseInt(hit_tv.getText());
+        if(hitValue>0){
+            hit_tv.setText(String.valueOf(hitValue-100));
+            sum_hit_tv.setText(String.valueOf(hitValue-100));
+        }
+    }
+
+    @FXML
+    private void sub_hit_value_sequence(){
+        sub_hit_btn.addEventHandler(MouseEvent.MOUSE_EXITED_TARGET, mouseEvent -> stopWatcher());
+        if(watcher_timeline == null){
+            watcher_timeline = new Timeline(new KeyFrame(Duration.seconds(0.2), actionEvent -> {
+                iv_border_1.setVisible(false);
+                iv_border_2.setVisible(false);
+                iv_border_3.setVisible(true);
+                int hitValue = Integer.parseInt(hit_tv.getText());
+                if(hitValue>0){
+                    hit_tv.setText(String.valueOf(hitValue-100));
+                    sum_hit_tv.setText(String.valueOf(hitValue-100));
+                }
+            })
+            );
+            watcher_timeline.setCycleCount(Timeline.INDEFINITE);
+            watcher_timeline.play();
+        }
     }
 }
